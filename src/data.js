a22b6980e4aad8c5f3e36d7da858e41354f530b3
FrankT <frank_training@outlook.com>
2022-10-15 22:11:10 -0500

    LIBREARY DATA.JS

--- src/data.js
+++ src/data.js
@@ -8,3 +8,8 @@ const accounts=JSON.parse(accountData);
 const userData=fs.readFileSync(
     path.join(__dirname,'json','users.json'),'utf-8');
 const users=JSON.parse(userData);
+
+function writeJSON(){
+    const accountsJSON=JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'),accountsJSON, 'utf-8');
+}
